ee070dfecf43d926380ebefd2edd1e85f00be774
ben mercer <benmercerdev@gmail.com>
2021-10-23 10:57:04 -0400

    play with structs

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -5,53 +5,45 @@ import "hardhat/console.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
-    // Mapping from address to uint
-    mapping(address => uint) public myMap;
-
-    function get(address _addr) public view returns (uint) {
-        // Mapping always returns a value.
-        // If the value was never set, it will return the default value.
-        return myMap[_addr];
+    struct Todo {
+        string text;
+        uint8 time;
+        bool completed;
     }
 
-    function set(address _addr, uint _i) public {
-        // Update the value at this address
-        myMap[_addr] = _i;
+    // An array of 'Todo' structs
+    Todo[] public todos;
+
+    function create(string memory _text, uint8 _time) public {
+        // 3 ways to initialize a struct
+        // - calling it like a function
+        todos.push(Todo(_text, _time, false));
+
+        // // key value mapping
+        // todos.push(Todo({text: _text, completed: false}));
+
+        // // initialize an empty struct and then update it
+        // Todo memory todo;
+        // todo.text = _text;
+        // // todo.completed initialized to false
+
+        // todos.push(todo);
     }
 
-    function remove(address _addr) public {
-        // Reset the value to the default value.
-        delete myMap[_addr];
+    // update text
+    function update(uint _index, string memory _text) public {
+        Todo storage todo = todos[_index];
+        todo.text = _text;
+    }
+
+    // toggle completed
+    function toggleCompleted(uint _index) public {
+        Todo storage todo = todos[_index];
+        todo.completed = !todo.completed;
+    }
+
+    function decrementTime(uint _index) public {
+        Todo storage todo = todos[_index];
+        todo.time--;
     }
 }
-
-// contract YourContract {
-
-//   bool public booBool = true;
-//   uint8 public u8 = 1;
-//   int public maxInt = type(int).max;
-
-//   //event SetPurpose(address sender, string purpose);
-
-//   string public purpose = "get rekt or lace moonboots";
-
-//   constructor() {
-//     // what should we do on deploy?
-//   }
-
-//   function setPurpose(string memory newPurpose) public {
-//       purpose = newPurpose;
-//       console.log(msg.sender,"set purpose to",purpose);
-//       //emit SetPurpose(msg.sender, purpose);
-//   }
-
-//   function setBooBool(bool newBool) public {
-//     booBool = newBool;
-//     console.log(msg.sender,"set booBool to",booBool);
-//   }
-
-//   function setU8 (uint8 newU8) public {
-//     u8 = newU8;
-//     console.log(msg.sender,"set u8 to", newU8);
-//   }
-// }
