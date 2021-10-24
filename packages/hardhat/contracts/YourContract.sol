2469f28d8bb59df5c017e9b04f105acb4e7622a4
ben mercer <benmercerdev@gmail.com>
2021-10-23 21:19:18 -0400

    modifiers play

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -5,45 +5,59 @@ import "hardhat/console.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
-    struct Todo {
-        string text;
-        uint8 time;
-        bool completed;
+    // We will use these variables to demonstrate how to use
+    // modifiers.
+    address public owner;
+    uint public x = 10;
+    bool public locked;
+
+    constructor() {
+        // Set the transaction sender as the owner of the contract.
+        owner = msg.sender;
     }
 
-    // An array of 'Todo' structs
-    Todo[] public todos;
-
-    function create(string memory _text, uint8 _time) public {
-        // 3 ways to initialize a struct
-        // - calling it like a function
-        todos.push(Todo(_text, _time, false));
-
-        // // key value mapping
-        // todos.push(Todo({text: _text, completed: false}));
-
-        // // initialize an empty struct and then update it
-        // Todo memory todo;
-        // todo.text = _text;
-        // // todo.completed initialized to false
-
-        // todos.push(todo);
+    // Modifier to check that the caller is the owner of
+    // the contract.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        // Underscore is a special character only used inside
+        // a function modifier and it tells Solidity to
+        // execute the rest of the code.
+        _;
     }
 
-    // update text
-    function update(uint _index, string memory _text) public {
-        Todo storage todo = todos[_index];
-        todo.text = _text;
+    // Modifiers can take inputs. This modifier checks that the
+    // address passed in is not the zero address.
+    modifier validAddress(address _addr) {
+        require(_addr != address(0), "Not valid address");
+        _;
     }
 
-    // toggle completed
-    function toggleCompleted(uint _index) public {
-        Todo storage todo = todos[_index];
-        todo.completed = !todo.completed;
+    function changeOwnerAny(address _newOwner) public validAddress(_newOwner) {
+        owner = _newOwner;
     }
 
-    function decrementTime(uint _index) public {
-        Todo storage todo = todos[_index];
-        todo.time--;
+    function changeOwnerRestricted(address _newOwner) public onlyOwner validAddress(_newOwner) {
+        owner = _newOwner;
+    }
+
+    // Modifiers can be called before and / or after a function.
+    // This modifier prevents a function from being called while
+    // it is still executing.
+    modifier noReentrancy() {
+        require(!locked, "No reentrancy");
+
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    // works if i <= 1
+    function decrement(uint i) public noReentrancy {
+        x -= i;
+
+        if (i > 1) {
+            decrement(i - 1);
+        }
     }
 }
