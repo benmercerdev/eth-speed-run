64b1e10cd17cbd75e207a8c8adf64befe0a03408
ben mercer <benmercerdev@gmail.com>
2021-10-23 23:44:12 -0400

    events and emitting play

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -5,59 +5,18 @@ import "hardhat/console.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
-    // We will use these variables to demonstrate how to use
-    // modifiers.
-    address public owner;
-    uint public x = 10;
-    bool public locked;
+  event SetPurpose(address sender, string purpose);
 
-    constructor() {
-        // Set the transaction sender as the owner of the contract.
-        owner = msg.sender;
-    }
+  string public purpose = "Building Unstoppable Apps!!!";
 
-    // Modifier to check that the caller is the owner of
-    // the contract.
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not owner");
-        // Underscore is a special character only used inside
-        // a function modifier and it tells Solidity to
-        // execute the rest of the code.
-        _;
-    }
+  constructor() {
+    emit SetPurpose(msg.sender, "Constructed!");
+    // what should we do on deploy?
+  }
 
-    // Modifiers can take inputs. This modifier checks that the
-    // address passed in is not the zero address.
-    modifier validAddress(address _addr) {
-        require(_addr != address(0), "Not valid address");
-        _;
-    }
-
-    function changeOwnerAny(address _newOwner) public validAddress(_newOwner) {
-        owner = _newOwner;
-    }
-
-    function changeOwnerRestricted(address _newOwner) public onlyOwner validAddress(_newOwner) {
-        owner = _newOwner;
-    }
-
-    // Modifiers can be called before and / or after a function.
-    // This modifier prevents a function from being called while
-    // it is still executing.
-    modifier noReentrancy() {
-        require(!locked, "No reentrancy");
-
-        locked = true;
-        _;
-        locked = false;
-    }
-
-    // works if i <= 1
-    function decrement(uint i) public noReentrancy {
-        x -= i;
-
-        if (i > 1) {
-            decrement(i - 1);
-        }
-    }
+  function setPurpose(string memory newPurpose) public {
+      purpose = newPurpose;
+      console.log(msg.sender,"set purpose to",purpose);
+      emit SetPurpose(msg.sender, purpose);
+  }
 }
